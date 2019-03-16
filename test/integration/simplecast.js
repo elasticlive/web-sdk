a2f4055af196372a478b659f37f9fa705c32001d
Jinho Choi <calmglow@gmail.com>
2019-03-16 13:05:57 +0900

    add integration test for simple call and cast. #1

---
+++ test/integration/simplecast.js
@@ -0,0 +1,8 @@
+"use strict";
+require(".")({
+  name: "SimpleCast",
+  path: "./test/integration/simplecast",
+  host: "localhost",
+  port: 5000,
+  timeout: 10000
+});
--- test/unit/index.js
+++ test/unit/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
 import "./config";
-// import "./auth";
+import "./auth";
 import "./elive";
 import "./signal";
